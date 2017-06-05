f1377aa53ab3d1897eb8e17454ce721d1efedf42
jephmann <jephmann@gmail.com>
2017-06-04 23:34:15 -0500

    tweaked meta description

--- _views/head.php
+++ _views/head.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="A personal workshop and demo project.">
+        <meta name="description" content="Jeffrey Hartmann's personal workshop and demo project.">
         <meta name="author" content="Jeffrey Hartmann">
         <title>Jeffrey Hartmann | <?php echo $subtitle; ?></title>
         <!--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
