1d20fefe4a47a64704fc1e4fd3d5d117fb8bd807
jephmann <jephmann@gmail.com>
2017-06-09 17:37:40 -0500

    Added HTML5 shiv

--- _views/head.php
+++ _views/head.php
@@ -10,5 +10,8 @@
         <!--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
         <link rel="stylesheet" href="<?php echo $path ?>_css/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo $path ?>_css/jephmann.css">
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <![endif]-->
     </head>
     <body>
