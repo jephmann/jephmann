71b21a1842e6fb6792f3ef09501c51e5cbf014e6
jephmann <jephmann@gmail.com>
2017-06-11 18:06:05 -0500

    Replaced photo; rewrote text

--- index.php
+++ index.php
@@ -15,12 +15,14 @@
             
             <div class="col-lg-5 col-md-5 col-sm-5">
 
-                <img src="_images/the-general-buster-keaton-3-540x362.jpg"
-                     alt="Buster Keaton"
+                <img src="_images/379395_2896829097182_85266811_n.jpg"
+                     alt="Jeffrey Hartmann"
                      width="100%" />
                 <blockquote class="blockquote-reverse">
-                    <p>Think slow, act fast.</p>
-                    <small>Buster Keaton</small>
+                    <p>Me on/about 2012. I still have most of this hair,
+                    but I now have new eyeglass frames. Here's hoping
+                    for a more recent image some day soon.</p>
+                    <small>Me</small>
                 </blockquote>
 
             </div>
@@ -30,35 +32,21 @@
                 <h2>Welcome to my workshop!</h2>
                 
                 <p>
-                    Why do I have a (hopefully public domain) picture of Buster
-                    Keaton instead of a picture of myself?
-                </p>
-                
-                <ul>
-                    <li>
-                        I'm not sure whether I want to be all that recognizable.
-                    </li>
-                    <li>
-                        I can't find anyone who would take a current photo of me.
-                    </li>
-                    <li>
-                        Plus, I prefer to work with monochromatic layouts,
-                        especially black-and-white, and such photos help me
-                        choose the right background colors for these pages.
-                    </li>
-                    <li>
-                        For a far more verbose explanation of this project,
+                    For a far more verbose explanation of this project,
                         you may click <a title="Jeffrey Hartmann: About"
                         href="about/">About</a>.
-                    </li>
-                    <li>
-                        By the way: Not to bury the lead or anything, but as I
-                        write this (May 2017), I would not mind a full-time,
-                        permanent job. You may contact me at <a target="_blank"
-                        title="LinkedIn: Jeffrey Hartmann"
-                        href="https://www.linkedin.com/in/jeffreyhartmann//">LinkedIn</a>.
-                    </li>
-                </ul>
+                </p>
+                <p>
+                    By the way: Not to bury the lead or anything, but as I
+                    write this (June 2017), I would not mind a full-time,
+                    permanent job. Feel free to contact me
+                    <em>privately</em> via <a target="_blank"
+                    title="LinkedIn: Jeffrey Hartmann"
+                    href="https://www.linkedin.com/in/jeffreyhartmann//">LinkedIn</a>.
+                </p>
+                <p>
+                    Thanks for visiting!
+                </p>
                 
             </div> 
             
