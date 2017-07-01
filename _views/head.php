bcf322509353871384172bcbceb1a3b9c94cfd4c
jephmann <jephmann@gmail.com>
2017-06-30 20:40:19 -0500

    Clarification: bootswatch.com "slate" version of Bootstrap

--- _views/head.php
+++ _views/head.php
@@ -7,7 +7,7 @@
         <meta name="description" content="Jeffrey Hartmann's personal workshop and demo project.">
         <meta name="author" content="Jeffrey Hartmann">
         <title>Jeffrey Hartmann | <?php echo $subtitle; ?></title>
-        <!--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
+        <!-- bootswatch.com "slate" version of bootstrap -->
         <link rel="stylesheet" href="<?php echo $path ?>_css/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo $path ?>_css/jephmann.css">
         <!--[if lt IE 9]>
