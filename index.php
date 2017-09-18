054ea9472e55ba496e64cd96be070b54ee6b6481
jephmann <jephmann@gmail.com>
2017-09-18 11:54:03 -0500

    Tweaked meta description

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
      *  Custom (per page) meta
      */
     $meta_image         = 'http://jephmann.com/_images/me201708.jpg';
-    $meta_description   = (string) NULL;
+    $meta_description   = 'Available for Hire | ';
     $meta_querystring   = (string) NULL;
     /*
      * HTML start
