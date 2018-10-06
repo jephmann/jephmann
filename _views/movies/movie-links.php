eb76fa25203ba6cb4d2b9e5c0df5fe8e66204271
jephmann <jephmann@gmail.com>
2018-10-06 02:23:23 -0500

    Tweaked movie-links section

--- _views/movies/movie-links.php
+++ _views/movies/movie-links.php
@@ -1,24 +1,28 @@
 
+        <h4>Additional information:</h4>
+        
         <p style="font-size: 1em;">
-            
-            <strong>See also:</strong><br />
             
             <a target="_blank"
                 title="The Movie Database page for <?php echo $logo_title; ?>"
-                href="<?php echo $overview[ 'urlMovieDB' ]; ?>">TMDb</a>
+                data-toggle="tooltip"
+                data-placement="top"
+                data-original-title="The Movie Database page for <?php echo $logo_title; ?>"
+                data-animation="false"
+                href="<?php echo $overview[ 'urlMovieDB' ]; ?>">The Movie Database</a>
             
             <?php if( !empty( $overview[ 'urlIMDB' ] ) ): ?>
             
-            &nbsp;|&nbsp;            
+            <br />            
             <a target="_blank"
                 title="Internet Movie Database page for <?php echo $logo_title; ?>"
-                href="<?php echo $overview[ 'urlIMDB' ]; ?>">IMDb</a> 
+                href="<?php echo $overview[ 'urlIMDB' ]; ?>">Internet Movie Database</a> 
                 
             <?php endif; ?>
             
             <?php if( !empty( $overview[ 'homepage' ] ) ): ?>
             
-            &nbsp;|&nbsp;            
+            <br />
             <a target="_blank"
                 title="Home page for <?php echo $logo_title; ?>"
                 href="<?php echo $overview[ 'homepage' ]; ?>">Web Site</a> 
