d5624399684720577f914a0e5f29ee8958cefde7
jephmann <jephmann@gmail.com>
2018-11-05 20:32:41 -0600

    jephmann.js created and applied to entire project

---
+++ _js/jephmann.js
@@ -0,0 +1,4 @@
+// for the upper-left Go Back button in navbar
+function goBack() {
+    window.history.back();
+}
--- _views/foot.php
+++ _views/foot.php
@@ -1,3 +1,4 @@
-
+        <script type="text/javascript"
+            src="<?php echo $path ?>_js/jephmann.js"></script>
     </body>
 </html>
