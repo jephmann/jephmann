95f528afb2f322078046fc9c3e774c0076df6371
jephmann <jephmann@gmail.com>
2018-11-22 02:37:57 -0600

    new self-portrait for OG card using LinkedIn dimensions

--- about/index.php
+++ about/index.php
@@ -8,7 +8,7 @@
     /*
      *  Custom (per page) meta
      */
-    $meta_image         = 'http://jephmann.com/_images/me201708_1200x675.jpg';
+    $meta_image         = 'http://jephmann.com/_images/me201708_LI.jpg';
     $meta_description   = (string) NULL;
     $meta_querystring   = (string) NULL;
     /*
