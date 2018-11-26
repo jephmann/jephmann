241ce5a7924fcdc9ff6e1bf59caca320242f5bf4
jephmann <jephmann@gmail.com>
2018-11-25 21:54:09 -0600

    Footer: Added Ko-fi link

--- _views/footer.php
+++ _views/footer.php
@@ -9,6 +9,11 @@
                 <p>
                     &copy; 2017 - <?php echo $thisYear; ?> by Jeffrey Hartmann
                 </p>
+                <p>
+                    <a href='https://ko-fi.com/S6S1MIEP' target='_blank'>
+                        <img height='36' style='border:0px;height:36px;' src='https://az743702.vo.msecnd.net/cdn/kofi2.png?v=0' border='0' alt='Buy Me a Coffee at ko-fi.com' />
+                    </a>
+                </p>
             </div>
         </div>
     </footer>
