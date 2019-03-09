cedd4da874d90d36b8af2a00b2a874e61f5add7f
jephmann <jephmann@gmail.com>
2019-03-09 16:13:23 -0600

    Movies/Search: widened middle column of results area

--- movies/api/index.php
+++ movies/api/index.php
@@ -122,12 +122,10 @@
 <div class="container">
     <div class="row">
 
-        <div class="col-lg-4 col-md-4 col-sm-4">
-            <?php
-            ?>
+        <div class="col-lg-3 col-md-3 col-sm-3">
         </div>
 
-        <div class="col-lg-4 col-md-4 col-sm-4">        
+        <div class="col-lg-6 col-md-6 col-sm-6">        
             <?php
                 require_once $views_search . 'name.php';
                 require_once $views_search . 'film.php';
@@ -136,9 +134,7 @@
             ?>
         </div>
 
-        <div class="col-lg-4 col-md-4 col-sm-4">
-            <?php
-            ?>
+        <div class="col-lg-3 col-md-3 col-sm-3">
         </div>
 
     </div>    
