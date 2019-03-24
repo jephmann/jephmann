8790eac42325ce3771c00bd9b22758c30bcf4871
jephmann <jephmann@gmail.com>
2019-03-24 17:48:35 -0500

    CDN links for ReactJS and Babel added; future use TBD

---
+++ _views/load/reactJS.php
@@ -0,0 +1,8 @@
+
+    <!-- load ReactJS and Babel -->
+    <script
+        src="https://unpkg.com/react@16/umd/react.development.js"></script>
+    <script
+        src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+    <script
+        src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
