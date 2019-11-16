7029d3afb7407cdc432dcf6446cb4d8957579d95
jephmann <jephmann@gmail.com>
2019-11-16 15:17:20 -0600

    Footer: removed obsolete image attributes from links

--- _views/footer.php
+++ _views/footer.php
@@ -13,26 +13,24 @@
                     <span>
                         <a target="_blank" href="https://dev.to/jeffreyhartmann">
                             <img
+                                id="imgDevTo"
                                 src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg"
                                 alt="Jeffrey Hartmann's DEV Profile"
                                 title="Jeffrey Hartmann's DEV Profile"                                
                                 data-toggle="tooltip" data-placement="top"
                                 data-original-title="Jeffrey Hartmann's DEV Profile"
-                                border="0"
-                                height="36"
                                 style="border: 0px; height: 36px;" />
                         </a>
                     </span>
                     <span>
                         <a target="_blank" href="https://ko-fi.com/S6S1MIEP">
                             <img
+                                id="imgKoFiCom"
                                 src="https://az743702.vo.msecnd.net/cdn/kofi2.png?v=0"
                                 alt="Buy Me a Coffee at ko-fi.com"
                                 title="Buy Me a Coffee at ko-fi.com"                                
                                 data-toggle="tooltip" data-placement="top"
                                 data-original-title="Buy Me a Coffee at ko-fi.com"
-                                border="0"
-                                height="36"
                                 style="border: 0px; height: 36px;" />
                         </a>
                     </span>
