f6cc2b5e2f81bb5282c33264aee237b55d68f27c
jephmann <jephmann@gmail.com>
2019-11-16 10:33:10 -0600

    Home Page: Reduced length of meta description

--- index.php
+++ index.php
@@ -12,9 +12,8 @@
      */
     $meta_image         = 'http://jephmann.com/_images/me201708_LI.jpg';
     $meta_description   = date('F Y')
-            . ': I am available for work,'
+            . ': Available for work,'
             . ' in and out of web development/design.'
-            . ' Chicago is home; however, relocation is an option.'
             . ' Please contact me privately for details. | ';
     $meta_querystring   = (string) NULL;
     /*
