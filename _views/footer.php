f58ad39ea2902a05f9415c31ca392f7e34c38ab1
jephmann <jephmann@gmail.com>
2019-11-19 17:31:04 -0600

    Footer: rebuilt footer and added WebWiki link for customer reviews

--- _views/footer.php
+++ _views/footer.php
@@ -1,40 +1,59 @@
 <div class="container">
     <footer>
         <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12">
+            <div class="col-lg-4 col-md-4 col-sm-4">
                 <p>
-                    Layout based on <a target="_blank"
-                      href="https://bootswatch.com/slate/">https://bootswatch.com/slate/</a>.
+                    Layout based on
+                    <a target="_blank" href="https://bootswatch.com/slate/"
+                        title="BootSwatch: Slate Template"                                
+                        data-toggle="tooltip" data-placement="top"
+                        data-original-title="BootSwatch: Slate Template">
+                        https://bootswatch.com/slate/
+                    </a>.
                 </p>
                 <p>
                     &copy; 2017 - <?php echo $thisYear; ?> by Jeffrey Hartmann
                 </p>
-                <div>
-                    <span>
-                        <a target="_blank" href="https://dev.to/jeffreyhartmann">
-                            <img
-                                id="imgDevTo"
-                                src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg"
-                                alt="Jeffrey Hartmann's DEV Profile"
-                                title="Jeffrey Hartmann's DEV Profile"                                
-                                data-toggle="tooltip" data-placement="top"
-                                data-original-title="Jeffrey Hartmann's DEV Profile"
-                                style="border: 0px; height: 36px;" />
-                        </a>
-                    </span>
-                    <span>
-                        <a target="_blank" href="https://ko-fi.com/S6S1MIEP">
-                            <img
-                                id="imgKoFiCom"
-                                src="https://az743702.vo.msecnd.net/cdn/kofi2.png?v=0"
-                                alt="Buy Me a Coffee at ko-fi.com"
-                                title="Buy Me a Coffee at ko-fi.com"                                
-                                data-toggle="tooltip" data-placement="top"
-                                data-original-title="Buy Me a Coffee at ko-fi.com"
-                                style="border: 0px; height: 36px;" />
-                        </a>
-                    </span>
-                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4">
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4" style="text-align: right">
+                <span>
+                    <a target="_blank" href="https://dev.to/jeffreyhartmann">
+                        <img
+                            id="imgDevTo"
+                            src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg"
+                            alt="Jeffrey Hartmann's DEV Profile"
+                            title="Jeffrey Hartmann's DEV Profile"                                
+                            data-toggle="tooltip" data-placement="top"
+                            data-original-title="Jeffrey Hartmann's DEV Profile"
+                            style="border: 0px; height: 36px;" />
+                    </a>
+                </span>
+                <span>
+                    <a target="_blank" href="https://ko-fi.com/S6S1MIEP">
+                        <img
+                            id="imgKoFiCom"
+                            src="https://az743702.vo.msecnd.net/cdn/kofi2.png?v=0"
+                            alt="Buy Me a Coffee at ko-fi.com"
+                            title="Buy Me a Coffee at ko-fi.com"                                
+                            data-toggle="tooltip" data-placement="top"
+                            data-original-title="Buy Me a Coffee at ko-fi.com"
+                            style="border: 0px; height: 36px;" />
+                    </a>
+                </span>
+                <span>
+                    <a target="_blank" href="https://www.webwiki.com/jephmann.com">
+                        <img
+                            id="imgWebWiki"
+                            src="https://www.webwiki.com/etc/rating/widget/1296048716/jephmann-com-rating-round-150.png"
+                            alt="Reviews of jephmann.com"
+                            title="Customer reviews of jephmann.com at WebWiki"                                
+                            data-toggle="tooltip" data-placement="top"
+                            data-original-title="Customer reviews of jephmann.com at WebWiki"
+                            style="border: 0px; height: 36px;" />
+                    </a>
+                </span>
             </div>
         </div>
     </footer>
