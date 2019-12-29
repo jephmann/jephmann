a71948001d05956b200799918e096c3a88e44eac
jephmann <jephmann@gmail.com>
2019-12-28 20:29:25 -0600

    Views/Foot: CookieInfoScript added (with all defaults)

--- _views/foot.php
+++ _views/foot.php
@@ -1,3 +1,7 @@
+        <!-- cookieinfoscript.com -->
+        <script type="text/javascript" id="cookieinfo"
+            src="//cookieinfoscript.com/js/cookieinfo.min.js"></script>
+        <!-- custom jephmann.com -->    
         <script type="text/javascript"
             src="<?php echo $path ?>_js/jephmann.js"></script>
     </body>
