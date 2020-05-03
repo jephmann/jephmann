1fc6934df66fc820ec3d42b476dc499b3369771f
jephmann <jephmann@gmail.com>
2020-05-02 20:42:16 -0500

    Movies: Honors table tidied up

--- _views/movies/omdbRatings.php
+++ _views/movies/omdbRatings.php
@@ -1,14 +1,14 @@
 <?php
-    if ( $omdb_awards or array_key_exists( 'Ratings', $omdb ) ) :
+    if( $omdb_awards or array_key_exists( 'Ratings', $omdb ) ) :
         
         if( array_key_exists( 'Ratings', $omdb ) ):
             ?><table class="table table-hover">
                 <caption>
                     <h4>Rankings and Honors</h4><?php
-                    if ( $overview[ 'imdb' ] )
+                    if( $overview[ 'imdb' ] )
                         require_once $path . '_plugins/imdb/ratings.php';
                 ?></caption><?php
-                foreach ( $omdb[ 'Ratings'] as $rating ) :
+                foreach( $omdb[ 'Ratings'] as $rating ) :
                     ?><tr class="table-primary">
                         <th scope="row"><?php echo $rating['Source']; ?></th>
                         <td><?php echo $rating['Value']; ?></td>
@@ -20,7 +20,7 @@
                         <td><?php echo $omdb_awards; ?></td>
                     </tr><?php
                 endif;
-                ?><tr><td colspan="2" style="text-align: right;"></td></tr>
+                ?>
             </table><?php
         endif;
 
