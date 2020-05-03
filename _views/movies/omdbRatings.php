66ffc75cfffa7786732e924f3f6b427bc1cef498
jephmann <jephmann@gmail.com>
2020-05-02 19:56:18 -0500

    Movies: IMDB plugin moved

--- _views/movies/omdbRatings.php
+++ _views/movies/omdbRatings.php
@@ -1,12 +1,13 @@
 <?php
     if ( $omdb_awards or array_key_exists( 'Ratings', $omdb ) ) :
-    
-        if ( $overview[ 'imdb' ] )
-            require_once $path . '_plugins/imdb/ratings.php';
         
         if( array_key_exists( 'Ratings', $omdb ) ):
             ?><table class="table table-hover">
-                <caption><h4>Rankings and Honors</h4></caption><?php
+                <caption>
+                    <h4>Rankings and Honors</h4><?php
+                    if ( $overview[ 'imdb' ] )
+                        require_once $path . '_plugins/imdb/ratings.php';
+                ?></caption><?php
                 foreach ( $omdb[ 'Ratings'] as $rating ) :
                     ?><tr class="table-primary">
                         <th scope="row"><?php echo $rating['Source']; ?></th>
@@ -19,7 +20,8 @@
                         <td><?php echo $omdb_awards; ?></td>
                     </tr><?php
                 endif;
-            ?></table><?php
+                ?><tr><td colspan="2" style="text-align: right;"></td></tr>
+            </table><?php
         endif;
 
     endif;
